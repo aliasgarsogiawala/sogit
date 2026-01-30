57dea655f56c5c70bd21ef0dcaab05f820113310
Aliasgar Sogiawala <vertexxinfotech18@gmail.com>
2026-01-30 11:06:23 +0530

    Touch up the init function and implement json

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
 import argparse
 import sys
 from pathlib import Path
+import json
 
 class Repository:
     def __init__(self, path="."):
         self.path = Path(path).resolve() #git init
         self.git_dir = self.path / ".sogit"
         #.git/objects
-        self.objects_dir= self.path / "objects"
+        self.objects_dir= self.git_dir / "objects"
         # .git/refs
-        self.ref_dir= self.path / "refs"
+        self.ref_dir= self.git_dir / "refs"
+        self.heads_dir= self.ref_dir / "refs"
         #HEAD FIle
         self.head_file= self.git_dir/"HEAD"
 
         #.git/index
         self.index_file= self.git_dir / "index"
     def init(self) -> bool:
+
+        #create directories
         self.git_dir.mkdir()
         self.objects_dir.mkdir()
         self.ref_dir.mkdir()
+
+        #HEAD file pointing to a default branch
+        self.heads_dir.mkdir()
         self.head_file.write_text("ref: refs/heads/main\n")
-        self.index_file.touch()
-        return True
+        self.index_file.write_text(json.dumps({},indent=2))
+        print(f"Initialised empty repository in {self.path}")
 
 
 
